93a8d1ca30beeec173ada184ed72b01dde2cf3d0
Adam Urbina <adamurbina@gmail.com>
2018-07-21 21:53:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
     end
 
     def self.find(id, db)
-        retrieve_pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?;",id)
+        db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?;",id)
         #new_id = retrieve_pokemon[0]
         #name = retrieve_pokemon[1]
         #type = retrieve_pokemon[2]
